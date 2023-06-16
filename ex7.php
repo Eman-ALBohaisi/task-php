0724986e52c3d7269e70df42cddf6a4b09670437
Eman Bassam <emanbassamalbohaisi@gmail.com>
2023-06-16 18:51:14 +0300

    first commit

---
+++ ex7.php
@@ -0,0 +1,12 @@
+<?php
+
+$string = "Emanbassam";
+
+$count = 0;
+for ($i = 0; $i < strlen($string); $i++) {
+    $char = substr($string, $i, 1);
+    if ($char >= 0 && $char <= 9) {
+        $count++;
+    }
+}
+echo $count;
